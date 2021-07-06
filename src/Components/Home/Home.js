197fe5e10f500b9121f3d72b47e6b7d2f92dafd4
Nicoll Oliver <nicolldon96@gmail.com>
2021-07-06 00:17:46 -0400

    Home component

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+    return (
+        <div className="Home">
+            <h1>Home Page</h1>
+            {/* Home Component */}
+        </div>
+    )
+}
+
+export default Home
