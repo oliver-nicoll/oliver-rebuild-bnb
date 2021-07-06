79c075feedd33b763aca68f9f5e73ad36564fd07
Nicoll Oliver <nicolldon96@gmail.com>
2021-07-06 00:26:36 -0400

    import banner

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Home.css'
+import Banner from '../Banner/Banner'
 
 function Home() {
     return (
         <div className="Home">
             <h1>Home Page</h1>
             {/* Home Component */}
+            <Banner />
         </div>
     )
 }
