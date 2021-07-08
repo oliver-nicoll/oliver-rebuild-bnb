915bd2a0d0b9f791bed96b2edef89f06242e704e
Nicoll Oliver <nicolldon96@gmail.com>
2021-07-08 10:31:19 -0400

    basic banner intro and button mui

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { Button } from '@material-ui/core'
 import './Banner.css'
 
 
 function Banner() {
     return (
         <div className="banner">
-            <h1>Banner</h1>
+            <div className="banner__info">
+                <h1>Get out and stretch your imagination</h1>
+                <h5>
+                    Plan a different kind of getaway to uncover the hidden gems near you with Oliver Rebuild LLC.
+                </h5>
+                <Button variant='outlined'>Explore Nearby</Button>
+            </div>
         </div>
     )
 }
