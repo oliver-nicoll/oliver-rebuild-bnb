129c093e7000ce9929b022ca5631f35aeda2cce8
Nicoll Oliver <nicolldon96@gmail.com>
2021-07-09 22:59:20 -0400

    card component make with props

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import "./Cards.css"
 
-function Cards() {
+function Cards({ src, title, description, price}) {
     return (
-        <div>
-            
+        <div className="card">
+            <img src={src} alt="" />
+            <div className="card__info">
+                <h2>{title}</h2>
+                    <h4> {description} </h4>
+                    <h3> {price} </h3>
+            </div>
         </div>
     )
 }
