186a531d8b09348c642352a195a0300db66f1b35
Nicoll Oliver <nicolldon96@gmail.com>
2021-07-13 22:41:01 -0400

    header fix

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import Logo from './Logo.png'
+
 import SearchIcon from '@material-ui/icons/Search';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import LanguageIcon from '@material-ui/icons/Language';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import './Header.css'
+import { Link } from "react-router-dom";
 
 function Header() {
     return (
         <div className="header">
-            <img 
-                className="header__icon" 
-                src={Logo} alt="logo" />
+              <Link to='/'>
+                <img
+                    className="header__icon"
+                    src={Logo}
+                    alt=""
+                />
+            </Link>
             <div className="header__center">
                 <input type="text" />
                 <SearchIcon />
